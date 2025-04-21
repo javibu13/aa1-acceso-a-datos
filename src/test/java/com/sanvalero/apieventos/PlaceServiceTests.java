4a4efbdc4d1f71147cbc44bdbacedccd571983a3
Javibu13 <javiersanzmoline@gmail.com>
2025-04-22 00:17:31 +0200

    Eliminar imports no necesarios

--- src/test/java/com/sanvalero/apieventos/PlaceServiceTests.java
+++ src/test/java/com/sanvalero/apieventos/PlaceServiceTests.java
@@ -1,13 +1,10 @@
 package com.sanvalero.apieventos;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyBoolean;
 import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
